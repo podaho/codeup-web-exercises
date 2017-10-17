69cb870080c81d5e52f5f7c35511300a8330790f
Po <po.grammer.lin@gmail.com>
2017-10-17 15:13:39 -0500

    Completed all loop bonus exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -28,10 +28,14 @@ function genTenRandos() {
 
 function cascadeNums() {
     var retString = "";
-    for(var i = 1; i < 10; i++) {
+    for(var i = 1; i <= 10; i++) {
         retString = "";
         for(var j = 0; j < i; j++) {
-            retString += i;
+            if(i === 10) {
+                retString += 0;
+            } else {
+                retString += i;
+            }
         }
         console.log(retString);
     }
@@ -42,3 +46,59 @@ function decreasingByFive() {
         console.log(100-(i*5));
     }
 }
+
+function diamond(input) {
+    if(input % 2 === 1) {
+        var i = 1;
+    } else {
+        var i = 2;
+    }
+    var holder = [];
+    var outStr = "";
+    while(i <= input) {
+        if(i < input) {
+            outStr += " ".repeat((input-i)/2);
+            outStr += "*".repeat(i);
+            outStr += " ".repeat((input-i)/2);
+            holder.push(outStr);
+        } else {
+            outStr += "*".repeat(i);
+        }
+        console.log(outStr);
+        outStr = "";
+        i += 2;
+    }
+    while(holder.length > 0) {
+        console.log(holder.pop());
+    }
+}
+
+function multTable(n) {
+    var numSpaces = (n*n).toString().length;
+    for(var i = 1; i <= n; i++) {
+        var outStr = "";
+        for(var j = 1; j <= n; j++) {
+            var tmp = (i*j).toString();
+            tmp += " ".repeat(numSpaces-tmp.length+1);
+            outStr += tmp;
+        }
+        console.log(outStr);
+    }
+}
+
+function fizzBuzz() {
+    for(var i = 1; i < 101; i++) {
+        var retStr = "";
+        if(i % 3 === 0 && i % 5 === 0) {
+            retStr = "FizzBuzz";
+        } else if(i % 3 === 0) {
+            retStr = "Fizz";
+        } else if(i % 5 === 0) {
+            retStr = "Buzz";
+        } else {
+            retStr += i;
+        }
+        console.log(retStr);
+    }
+}
+
