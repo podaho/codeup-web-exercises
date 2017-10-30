c06e23576dd6a346693b4aebda555a1a2c53b135
Po <po.grammer.lin@gmail.com>
2017-10-30 15:28:09 -0500

    Completed jQuery Mouse Events Exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* //jQuery Introduction Exercise
 $(document).ready(function() {
     alert("DOM ready for manipulation!");
@@ -15,6 +17,32 @@ console.log(sameIdSelectors);
 $('.codeup').css('border', '1px solid red'); //This only selects class names of 'codeup', IDs will not be selected even if the name is identical
 */
 
-$('li').css('font-size','30px');
+/* //jQuery Element/Multiple Selectors Exercise
+$('li').first().css({
+    'font-size': '40px',
+    'font-weight': 'bolder'
+});
 $('h1, p, li').css('background-color','yellow');
 alert($('li').html());
+*/
+
+$('h1').first().click(function(e) {
+    $(e.target).css({
+        'background-color': 'yellow'
+    });
+});
+$('p').dblclick(function() {
+    $(this).css('font-size', '18px');
+});
+function toRed(e) {
+    $(e.target).css({
+        'color': 'red'
+    });
+}
+function toBlack() {
+    $(this).css({
+        'color': 'black'
+    });
+}
+$('li').hover(toRed,toBlack);
+
