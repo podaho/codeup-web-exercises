bd40121bf16331a9b8e0a297c7f6a6380a22ab5a
Po <po.grammer.lin@gmail.com>
2017-10-30 14:18:57 -0500

    Completed jQuery Selectors Exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,20 @@
+/* //jQuery Introduction Exercise
 $(document).ready(function() {
     alert("DOM ready for manipulation!");
-})
+});
+*/
+
+/* //jQuery Id Selectors Exercise
+alert($('#first-paragraph').html());
+alert($('#only-div').html());
+var sameIdSelectors = $('#important').html(); //this will only show the first occurance of the id selector
+console.log(sameIdSelectors);
+*/
+
+/* //jQuery Class Selectors Exercise
+$('.codeup').css('border', '1px solid red'); //This only selects class names of 'codeup', IDs will not be selected even if the name is identical
+*/
+
+$('li').css('font-size','30px');
+$('h1, p, li').css('background-color','yellow');
+alert($('li').html());
